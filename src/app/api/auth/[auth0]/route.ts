7707db10454bc1e0ccbbf24866de9b45a002bc3f
drqxyzz <not.dark69@gmail.com>
2025-11-25 06:23:42 +0000

    Fix login download: Correctly wrap AuthClient handler

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -2,5 +2,5 @@ import { AuthClient } from "@auth0/nextjs-auth0/server";
 
 const client = new AuthClient({} as any);
 
-export const GET = client.handler;
-export const POST = client.handler;
+export const GET = (req: any, ctx: any) => client.handler(req);
+export const POST = (req: any, ctx: any) => client.handler(req);
